ff5d89f19e5fac7653ae9e529487ed334d92d01e
Sergio-Cordeiro <62566660+Sergio-Cordeiro@users.noreply.github.com>
2020-10-12 19:12:44 -0300

    new graphic to mouthResults

--- SOMEI/SOMEI/Controllers/Financial/MounthReport/MouthResultViewController.swift
+++ SOMEI/SOMEI/Controllers/Financial/MounthReport/MouthResultViewController.swift
@@ -9,14 +9,42 @@
 import UIKit
 
 class MouthResultViewController: UIViewController {
-
+    
+    @IBOutlet weak var borderView: UIView!
+    
+    @IBOutlet weak var saldoAtualNumber: UILabel!
+    @IBOutlet weak var metaMensalNumber: UILabel!
+    @IBOutlet weak var previsaoMensalNumber: UILabel!
+    
+    @IBOutlet weak var viewGraphics: BarChart!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureLayoutView()
+        atualizarGrafico()
+    }
+    
+    func configureLayoutView() {
+        borderView.clipsToBounds = false
+        borderView.backgroundColor = UIColor.white
+        borderView.layer.shadowColor = UIColor.black.cgColor
+        borderView.layer.shadowOpacity = 0.14
+        borderView.layer.shadowOffset = .zero
+        borderView.layer.shadowRadius = 3
+        borderView.layer.cornerRadius = 10
+    }
+    
+    func atualizarGrafico() {
+        
+            let info1 = 0, info2 = 50, info3 = 20, info4 = 30, info5 = 10
+                
+                
+            viewGraphics.atualizar([info1,info2,info3,info4,info5])
        
     }
-    func configureLayoutView() {
-        
+    
+    @IBAction func backButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
-
+    
 }
---
+++ SOMEI/SOMEI/View/UIComponents/BarChart.swift
@@ -0,0 +1,88 @@
+//
+//  BarChart.swift
+//  SOMEI
+//
+//  Created by Sergio Cordeiro on 12/10/20.
+//  Copyright © 2020 José Guilherme Bestel. All rights reserved.
+//
+
+import UIKit
+
+class BarChart: UIView {
+
+    var dados = [0,0,0,0,0]
+    var context: CGContext!
+  
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        context = UIGraphicsGetCurrentContext()!
+        desenharOsEixos()
+        desenharAsBarras()
+    }
+    
+    private func desenharOsEixos(){
+            
+        //Eixos horizontais
+        context.setStrokeColor(UIColor.lightGray.cgColor)
+        context.setLineWidth(0.5)
+        context.beginPath()
+        var espacoEntreOsEixos = bounds.height*0.9/5
+        var eixoAtual = bounds.height*0.05
+        while eixoAtual < bounds.height {
+            context.move(to: CGPoint(x: 0, y: eixoAtual))
+            context.addLine(to: CGPoint(x: bounds.width*0.9, y:eixoAtual))
+            eixoAtual += espacoEntreOsEixos
+        }
+        context.strokePath()
+        
+        //Eixos verticais
+        context.beginPath()
+        context.setLineDash(phase: 0, lengths: [1,2])
+        espacoEntreOsEixos = bounds.width*0.9/11
+        eixoAtual = espacoEntreOsEixos
+        while eixoAtual <= bounds.width*0.9{
+            context.move(to: CGPoint(x: eixoAtual, y: 0))
+            context.addLine(to: CGPoint(x: eixoAtual, y:bounds.height-bounds.height*0.05))
+            eixoAtual += espacoEntreOsEixos
+        }
+        context.strokePath()
+    }
+    
+    private func desenharAsBarras(){
+        
+            context.setStrokeColor(UIColor.blue.cgColor)
+            context.setLineWidth(bounds.width*0.9/11)
+            context.setLineDash(phase: 0, lengths: [])
+            context.beginPath()
+            let espacoEntreOsEixos = bounds.width*0.9/11
+            var eixoAtual = espacoEntreOsEixos + espacoEntreOsEixos/2
+            for i in dados{
+                context.move(to: CGPoint(x: eixoAtual, y: bounds.height*0.95))
+                context.addLine(to: CGPoint(x: eixoAtual, y: bounds.height*0.95-bounds.height*0.90/CGFloat(valorMaximo())*CGFloat(i)))
+                eixoAtual += espacoEntreOsEixos*2
+            }
+            context.strokePath()
+        
+    }
+    
+    private func valorMaximo() -> Int {
+        
+            var maior = 5
+            for valor in dados{
+                if valor > maior{
+                    maior = valor
+                }
+            }
+            while maior % 5 != 0{
+                maior += 1
+            }
+            return maior
+    }
+    
+    public func atualizar(_ dados: [Int]){
+        self.dados = dados
+        self.subviews.forEach { $0.removeFromSuperview() }
+        self.setNeedsDisplay()
+    }
+
+}
